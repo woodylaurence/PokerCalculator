fbad6fe66f422b8930749ae40fa0fe4ee66544ce
Laurence Woodward <woodylaurence90@gmail.com>
2018-02-17 12:54:49 +0000

    Added PokerOdds object and tests.

---
+++ Domain/PokerObjects/PokerOdds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PokerCalculator.Domain.PokerObjects
+{
+	public class PokerOdds
+	{
+		public double WinPercentage { get; set; }
+		public double DrawPercentage { get; set; }
+		public double LosePercentage { get; set; }
+
+		public Dictionary<PokerEnums.PokerHand, double> PokerHandPercentages { get; } = Utilities.GetEnumValues<PokerEnums.PokerHand>().ToDictionary(x => x, x => 0d);
+	}
+}
---
+++ Tests - Integration/PokerObjects/PokerOddsIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using PokerCalculator.Domain.PokerEnums;
+using PokerCalculator.Domain.PokerObjects;
+
+namespace PokerCalculator.Tests.Integration.PokerObjects
+{
+	[TestFixture]
+	public class PokerOddsIntegrationTests : LocalTestBase
+	{
+		[Test]
+		public void Constructor()
+		{
+			//act
+			var actual = new PokerOdds();
+
+			//assert
+			Assert.That(actual.WinPercentage, Is.EqualTo(0));
+			Assert.That(actual.DrawPercentage, Is.EqualTo(0));
+			Assert.That(actual.LosePercentage, Is.EqualTo(0));
+
+			Assert.That(actual.PokerHandPercentages, Has.Count.EqualTo(10));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.RoyalFlush));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.StraightFlush));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.FourOfAKind));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.FullHouse));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.Flush));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.Straight));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.ThreeOfAKind));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.TwoPair));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.Pair));
+			Assert.That(actual.PokerHandPercentages.ContainsKey(PokerHand.HighCard));
+
+			Assert.That(actual.PokerHandPercentages.Values, Has.All.EqualTo(0));
+		}
+	}
+}
