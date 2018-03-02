43b49551ee0cd2c97110e5bc165b4afb3acf4ad5
Laurence Woodward <woodylaurence90@gmail.com>
2018-03-02 14:26:50 +0000

    Added ability to construct card using a string.

--- Domain/PokerObjects/Card.cs
+++ Domain/PokerObjects/Card.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PokerCalculator.Domain.Helpers;
 using PokerCalculator.Domain.PokerEnums;
+using System;
+using System.Text.RegularExpressions;
 
 namespace PokerCalculator.Domain.PokerObjects
 {
@@ -14,15 +16,33 @@ namespace PokerCalculator.Domain.PokerObjects
 
 		#region Constructors
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="suit"></param>
 		public Card(CardValue value, CardSuit suit)
 		{
 			Value = value;
 			Suit = suit;
 		}
 
-		public Card(string cardAsString)
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="cardAsString"></param>
+		/// <param name="utilitiesService"></param>
+		public Card(string cardAsString, IUtilitiesService utilitiesService)
 		{
-			throw new NotImplementedException();
+			if (string.IsNullOrWhiteSpace(cardAsString)) throw new ArgumentException("Must provide string representation of Card");
+
+			var cardRegex = new Regex("^([AKQJ2-9]|10)([SHDC])$", RegexOptions.IgnoreCase);
+			if (cardRegex.IsMatch(cardAsString) == false) throw new ArgumentException("Supplied string does not conform to allowed Card values");
+			var cardValueAsString = cardRegex.Match(cardAsString).Groups[1].Value.ToUpper();
+			var cardSuitAsString = cardRegex.Match(cardAsString).Groups[2].Value.ToUpper();
+
+			Value = utilitiesService.GetEnumValueFromDescription<CardValue>(cardValueAsString);
+			Suit = utilitiesService.GetEnumValueFromDescription<CardSuit>(cardSuitAsString);
 		}
 
 		#endregion
--- Tests - Unit/PokerObjects/CardUnitTests.cs
+++ Tests - Unit/PokerObjects/CardUnitTests.cs
@@ -1,14 +1,27 @@
 ﻿using NUnit.Framework;
+using PokerCalculator.Domain.Helpers;
 using PokerCalculator.Domain.PokerEnums;
 using PokerCalculator.Domain.PokerObjects;
 using PokerCalculator.Tests.Shared;
 using PokerCalculator.Tests.Shared.TestData;
+using Rhino.Mocks;
+using System;
 
 namespace PokerCalculator.Tests.Unit.PokerObjects
 {
 	[TestFixture]
 	public class CardUnitTests : AbstractUnitTestBase
 	{
+		private IUtilitiesService _utilitiesService;
+
+		[SetUp]
+		protected override void Setup()
+		{
+			base.Setup();
+
+			_utilitiesService = MockRepository.GenerateStrictMock<IUtilitiesService>();
+		}
+
 		#region Constructor
 
 		[Test, TestCaseSource(typeof(CardTestCaseData), nameof(CardTestCaseData.AllCardsTestCaseData))]
@@ -22,16 +35,58 @@ namespace PokerCalculator.Tests.Unit.PokerObjects
 			Assert.That(actual.Suit, Is.EqualTo(suit));
 		}
 
-		[Test, TestCaseSource(typeof(CardTestCaseData), nameof(CardTestCaseData.AllCardsAsString))]
-		public void Constructor_string(string cardAsString, CardValue expectedCardValue, CardSuit expectedCardSuit)
+		[TestCase("")]
+		[TestCase("   ")]
+		[TestCase(null)]
+		public void Constructor_string_WHERE_string_is_null_or_whitespace_SHOULD_throw_error(string cardAsString)
+		{
+			//act + assert
+			var actualException = Assert.Throws<ArgumentException>(() => new Card(cardAsString, _utilitiesService));
+			Assert.That(actualException.Message, Is.EqualTo("Must provide string representation of Card"));
+		}
+
+		[TestCase("TD")]
+		[TestCase("11S")]
+		[TestCase("9G")]
+		[TestCase("9Clubs")]
+		[TestCase("0D")]
+		[TestCase("asjhgfas")]
+		public void Constructor_string_WHERE_string_does_not_conform_to_card_regex_SHOULD_throw_error(string input)
 		{
 			//act
-			var actual = new Card(cardAsString);
+			var actualException = Assert.Throws<ArgumentException>(() => new Card(input, _utilitiesService));
+			Assert.That(actualException.Message, Is.EqualTo("Supplied string does not conform to allowed Card values"));
+		}
+
+		[TestCase("4", "D")]
+		[TestCase("10", "S")]
+		[TestCase("a", "H")]
+		[TestCase("K", "H")]
+		[TestCase("j", "S")]
+		[TestCase("Q", "C")]
+		[TestCase("4", "C")]
+		public void Constructor_string(string cardValueAsString, string cardSuitAsString)
+		{
+			//arrange
+			var cardAsString = $"{cardValueAsString}{cardSuitAsString}";
+
+			var cardValueAsStringUppercase = cardValueAsString.ToUpper();
+			var cardSuitAsStringUppercase = cardSuitAsString.ToUpper();
+
+			const CardValue expectedValue = CardValue.Jack;
+			_utilitiesService.Stub(x => x.GetEnumValueFromDescription<CardValue>(cardValueAsStringUppercase)).Return(expectedValue);
+
+			const CardSuit expectedSuit = CardSuit.Hearts;
+			_utilitiesService.Stub(x => x.GetEnumValueFromDescription<CardSuit>(cardSuitAsStringUppercase)).Return(expectedSuit);
+
+			//act
+			var actual = new Card(cardAsString, _utilitiesService);
 
 			//assert
-			Assert.That(actual.Value, Is.EqualTo(expectedCardValue));
-			Assert.That(actual.Suit, Is.EqualTo(expectedCardValue));
+			Assert.That(actual.Value, Is.EqualTo(expectedValue));
+			Assert.That(actual.Suit, Is.EqualTo(expectedSuit));
 		}
+
 
 		#endregion
 	}
