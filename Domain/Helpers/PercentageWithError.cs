eb62755f82bbc46735dc08bcc21b6c25e3b7df2e
Laurence Woodward <woodylaurence90@gmail.com>
2018-03-23 23:19:49 +0000

    Added PercentageWithError so that now all results are given a margin of error.

---
+++ Domain/Helpers/PercentageWithError.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokerCalculator.Domain.Helpers
+{
+	public class PercentageWithError
+	{
+		#region Properties and Fields
+
+		public double Percentage { get; set; }
+		public double Error { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		///
+		/// </summary>
+		/// <param name="percentage"></param>
+		/// <param name="error"></param>
+		internal PercentageWithError(double percentage, double error)
+		{
+			Percentage = percentage;
+			Error = error;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="percentages"></param>
+		public PercentageWithError(List<double> percentages)
+		{
+			if (percentages.Count < 2) throw new ArgumentException("Cannot calculate error with less than two percentages");
+
+			var mean = percentages.Average();
+			var variance = percentages.Average(x => (x - mean) * (x - mean));
+			Percentage = mean;
+			Error = Math.Sqrt(variance);
+		}
+
+		#endregion
+
+		#region Instance Methods
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			return Error == 0
+						? $"{Percentage * 100}%"
+						: $"{Percentage:P1} ± {Error:P1}";
+		}
+
+		#endregion
+	}
+}
--- Domain/PokerCalculator/PokerOdds.cs
+++ Domain/PokerCalculator/PokerOdds.cs
@@ -1,4 +1,6 @@
-﻿using PokerCalculator.Domain.Helpers;
+﻿using System;
+using Microsoft.Practices.ServiceLocation;
+using PokerCalculator.Domain.Helpers;
 using PokerCalculator.Domain.PokerEnums;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +11,28 @@ namespace PokerCalculator.Domain.PokerCalculator
 	{
 		#region Properties and Fields
 
-		internal virtual int NumWins { get; set; }
-		internal virtual int NumDraws { get; set; }
-		internal virtual int NumLosses { get; set; }
-		internal virtual int TotalNumHands => NumWins + NumDraws + NumLosses;
+		internal static PokerOdds MethodObject = new PokerOdds(new UtilitiesService());
 
-		public virtual double WinPercentage => TotalNumHands == 0 ? 0 : (double)NumWins / TotalNumHands;
-		public virtual double DrawPercentage => TotalNumHands == 0 ? 0 : (double)NumDraws / TotalNumHands;
-		public virtual double LossPercentage => TotalNumHands == 0 ? 0 : (double)NumLosses / TotalNumHands;
+		protected internal virtual int NumWins { get; set; }
+		protected internal virtual int NumDraws { get; set; }
+		protected internal virtual int NumLosses { get; set; }
+		protected internal virtual int TotalNumHands => NumWins + NumDraws + NumLosses;
 
-		internal virtual Dictionary<PokerHand, int> PokerHandFrequencies { get; }
-		public virtual Dictionary<PokerHand, double> PokerHandPercentages => PokerHandFrequencies.ToDictionary(x => x.Key, x => TotalNumHands == 0 ? 0 : (double)x.Value / TotalNumHands);
+		protected internal virtual double WinPercentage => TotalNumHands == 0 ? 0 : (double)NumWins / TotalNumHands;
+		protected internal virtual double DrawPercentage => TotalNumHands == 0 ? 0 : (double)NumDraws / TotalNumHands;
+		protected internal virtual double LossPercentage => TotalNumHands == 0 ? 0 : (double)NumLosses / TotalNumHands;
+
+		protected internal virtual Dictionary<PokerHand, int> PokerHandFrequencies { get; internal set; }
+		protected internal virtual Dictionary<PokerHand, double> PokerHandPercentages => PokerHandFrequencies.ToDictionary(x => x.Key, x => TotalNumHands == 0 ? 0 : (double)x.Value / TotalNumHands);
+
+		#region Aggregated Values
+
+		public virtual PercentageWithError WinPercentageWithError { get; private set; }
+		public virtual PercentageWithError DrawPercentageWithError { get; private set; }
+		public virtual PercentageWithError LossPercentageWithError { get; private set; }
+		public virtual Dictionary<PokerHand, PercentageWithError> PokerHandPercentagesWithErrors { get; internal set; }
+
+		#endregion
 
 		#endregion
 
@@ -28,6 +41,32 @@ namespace PokerCalculator.Domain.PokerCalculator
 		public PokerOdds(IUtilitiesService utilitiesService)
 		{
 			PokerHandFrequencies = utilitiesService.GetEnumValues<PokerHand>().ToDictionary(x => x, x => 0);
+		}
+
+		#endregion
+
+		#region Static Methods
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pokerOdds"></param>
+		/// <returns></returns>
+		public static PokerOdds AggregatePokerOdds(List<PokerOdds> pokerOdds) => MethodObject.AggregatePokerOddsSlave(pokerOdds);
+		protected internal virtual PokerOdds AggregatePokerOddsSlave(List<PokerOdds> pokerOdds)
+		{
+			if (pokerOdds.Count < 2) throw new ArgumentException("Cannot aggregate less than two PokerOdds.");
+
+			var utilitiesService = ServiceLocator.Current.GetInstance<IUtilitiesService>();
+			return new PokerOdds(utilitiesService)
+			{
+				WinPercentageWithError = new PercentageWithError(pokerOdds.Select(x => x.WinPercentage).ToList()),
+				DrawPercentageWithError = new PercentageWithError(pokerOdds.Select(x => x.DrawPercentage).ToList()),
+				LossPercentageWithError = new PercentageWithError(pokerOdds.Select(x => x.LossPercentage).ToList()),
+				PokerHandPercentagesWithErrors = pokerOdds.SelectMany(x => x.PokerHandPercentages)
+														  .GroupBy(x => x.Key)
+														  .ToDictionary(x => x.Key, x => new PercentageWithError(x.Select(y => y.Value).ToList()))
+			};
 		}
 
 		#endregion
